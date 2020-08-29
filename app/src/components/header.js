76bb3e776de98cebfc55db4641ca56537450d420
Marc-Philippe Huget <mphuget@gmail.com>
2020-08-29 22:27:22 +0200

    Change the header style

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../style/header.css';
 
 function Header() {
   return (
     <div className="Header">
-      <h1> Header </h1>
+      <h1> My list of Todo </h1>
     </div>
   );
 }
